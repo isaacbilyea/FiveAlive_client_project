bfced88f57e69e6344fb6f12b938d234507a6de3
Isaac Bilyea <isaacbilyea@gmail.com>
2025-03-05 20:21:36 -0500

    Validate HTML/CSS
    Validate HTML/CSS

--- js/modules/mission-buttons.js
+++ js/modules/mission-buttons.js
@@ -1,19 +1,25 @@
 export function missionButtons() {
-    
+
+    //VARIABLES
     const missionButtons = document.querySelectorAll('.read-more-btn, .arrow');
 
-    missionButtons.forEach(element => {
-        element.addEventListener('click', () => {
-            const missionCard = element.closest('.mission');
-            const button = missionCard.querySelector('.read-more-btn');
-            
-            missionCard.classList.toggle('expanded');
-            
-            if (missionCard.classList.contains('expanded')) {
-                button.textContent = 'Read Less';
-            } else {
-                button.textContent = 'Read More';
-            }
-        });
+    //FUNCTIONS
+    function toggleMissionContent(e) {
+        const missionCard = e.currentTarget.closest('.mission');
+        const button = missionCard.querySelector('.read-more-btn');
+        
+        missionCard.classList.toggle('expanded');
+        
+        if (missionCard.classList.contains('expanded')) {
+            button.textContent = 'Read Less';
+        } else {
+            button.textContent = 'Read More';
+        }
+    }
+
+    //EVENT LISTENERS
+    missionButtons.forEach(button => {
+        button.addEventListener('click', toggleMissionContent);
     });
+
 }
