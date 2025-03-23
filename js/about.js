6a1e09dbb31a457fe3785ebb5250d36c5270a307
Isaac Bilyea <isaacbilyea@gmail.com>
2025-03-22 21:08:08 -0400

    Update desktop styling for more battles page
    Update desktop styling for more battles page

--- js/about.js
+++ js/about.js
@@ -1,7 +1,5 @@
 import { burgerMenu } from "./modules/burger-menu.js";
-import { missionButtons } from "./modules/mission-buttons.js";
 import { videoPlayer } from "./modules/video-player.js";
 
 burgerMenu();
-missionButtons();
 videoPlayer();
--- js/modules/battle-filter.js
+++ js/modules/battle-filter.js
@@ -1,22 +1,22 @@
 export function battleFilter() {
+    //VARIABLES
     const filterButtons = document.querySelectorAll('#filter-bar button');
     const battleGroups = document.querySelectorAll('.battle-group');
 
-    function filterBattles(clickedButton) {
+    //FUNCTIONS
+    function filterBattles(e) {
         filterButtons.forEach(btn => btn.classList.remove('active'));
         battleGroups.forEach(group => group.classList.remove('active'));
         
-        clickedButton.classList.add('active');
+        e.classList.add('active');
         
-        const campaign = clickedButton.dataset.campaign;
+        const campaign = e.dataset.campaign;
         const selectedGroup = document.querySelector(`.battle-group[data-campaign="${campaign}"]`);
-        if (selectedGroup) selectedGroup.classList.add('active');
+        selectedGroup.classList.add('active');
     }
 
+    //EVENT LISTENERS
     filterButtons.forEach(button => {
         button.addEventListener('click', () => filterBattles(button));
     });
-
-    // Initialize first battle group
-    filterButtons[0].click();
 }
