30117147bf7abcb5e01dba7a62a9bb9d23d9c24f
Isaac Bilyea <isaacbilyea@gmail.com>
2025-03-26 15:43:25 -0400

    Added more GSAP animations

--- js/modules/content-filter.js
+++ js/modules/content-filter.js
@@ -1,23 +1,34 @@
+
 export function contentFilter() {
-    //VARIABLES
     const filterButtons = document.querySelectorAll('#filter-bar button');
     const filterGroups = document.querySelectorAll('.filter-content');
 
-    //FUNCTIONS
     function filterContent(e) {
         filterButtons.forEach(btn => btn.classList.remove('active'));
-        filterGroups.forEach(group => group.classList.remove('active'));
-        
+        filterGroups.forEach(group => {
+            group.classList.remove('active');
+            group.style.display = 'none';
+        });
+
         e.classList.add('active');
-        
+
         const type = e.dataset.type;
         const selectedGroup = document.querySelector(`.filter-content[data-type="${type}"]`);
-        if (selectedGroup) selectedGroup.classList.add('active');
+
+        selectedGroup.style.display = 'flex';
+        selectedGroup.classList.add('active');
+
+        gsap.to(window, {
+            duration: 1,
+            scrollTo: {
+                y: selectedGroup,
+                offsetY: 150
+            },
+            ease: "power2.out"
+        });
     }
 
-    //EVENT LISTENERS
     filterButtons.forEach(button => {
         button.addEventListener('click', () => filterContent(button));
     });
-
 }
--- js/modules/gsap-sections.js
+++ js/modules/gsap-sections.js
@@ -1,13 +1,17 @@
 export function gsapSections() {
     gsap.registerPlugin(ScrollTrigger);
     
-    const sections = document.querySelectorAll('section');
+    const sections = document.querySelectorAll('section:not(.subpage-hero-section):not(.no-animation)');
 
     sections.forEach(section => {
-        gsap.from(section, {
+
+        const elements = section.querySelectorAll(':scope > *:not(.no-animation)');
+        
+        gsap.from(elements, {
             y: 25,
             opacity: 0,
             duration: 1,
+            stagger: 0.2,
             scrollTrigger: {
                 trigger: section,
                 start: "top 80%",
@@ -15,4 +19,20 @@ export function gsapSections() {
             }
         });
     });
+
+    // const backgrounds = document.querySelectorAll('.bg-tertiary, .bg-secondary');
+    // if(backgrounds) {
+    //     backgrounds.forEach(background => {
+    //         gsap.from(background, {
+    //             y: 25,
+    //             opacity: 0,
+    //             duration: 1,
+    //             scrollTrigger: {
+    //                 trigger: background,
+    //                 start: "top 80%",
+    //                 toggleActions: "play none none reverse"
+    //             }
+    //         });
+    //     });
+    // }
 }
