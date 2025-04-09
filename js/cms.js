f1ae9bb93eaecfe34b3c2ecdca73bc9c259cd1f7
Isaac Bilyea <isaacbilyea@gmail.com>
2025-04-08 21:26:22 -0400

    Add initial cms styling

---
+++ js/cms.js
@@ -0,0 +1,12 @@
+import { adminNews } from './modules/admin.js';
+import { adminEvents } from './modules/admin.js';
+import { adminVolunteers } from './modules/admin.js';
+import { adminDonors } from './modules/admin.js';
+import { adminNewsletters } from './modules/admin.js';
+
+
+adminNews();
+adminEvents();
+adminVolunteers();
+adminDonors();
+adminNewsletters();
