aba02815a7cac51428d77de5410970e3757bd221
Isaac Bilyea <isaacbilyea@gmail.com>
2025-04-10 06:44:48 -0400

    Add walkthrough and versioning
    Add walkthrough folder with 360 experience and css/js file versioning for live site

--- js/modules/gsap-thank.js
+++ js/modules/gsap-thank.js
@@ -4,14 +4,14 @@ export function gsapThankyou() {
     timeline.from("#thankyou", { 
         opacity: 0, 
         y: -50, 
-        duration: 0.8, 
+        duration: 0.4, 
         ease: "power2.out" 
     });
 
     timeline.from(".info", {
         opacity: 0,
         y: 30,
-        duration: 0.6,
+        duration: 0.4,
         ease: "power2.out",
         stagger: 0.4
     });
@@ -20,7 +20,7 @@ export function gsapThankyou() {
         opacity: 0,
         scaleX: 0,
         transformOrigin: "left center",
-        duration: 0.5,
+        duration: 0.3,
         ease: "power2.out",
         stagger: 0.2
     });
@@ -28,7 +28,7 @@ export function gsapThankyou() {
     timeline.from("#thankyou-image", { 
         opacity: 0, 
         y: 50, 
-        duration: 1.2,
+        duration: 1,
         ease: "power3.out",
-    });
+    }, "<");
 }
---
+++ walkthrough/data.js
@@ -0,0 +1,166 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-park-sign",
+      "name": "Park Sign",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sponsorship-opportunity",
+      "name": "Sponsorship Opportunity",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pond-lookout",
+      "name": "Pond Lookout",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-interpretive-trail-signage",
+      "name": "Interpretive Trail Signage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-brothers-in-arms-memorial",
+      "name": "Brothers In Arms Memorial",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
